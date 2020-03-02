10ab7c13cdc767013445a360321a80fa91da4ec4
Jonathan Hughes <jonathanbhughes@gmail.com>
2020-03-02 22:50:34 +0000

    Store visited links in localStorage

--- src/modules/my/item/item.js
+++ src/modules/my/item/item.js
@@ -1,10 +1,22 @@
 import { LightningElement, api } from 'lwc';
 const AVAILABLE_ICONS = ['article', 'guide', 'pdf'];
+const LOCAL_STORAGE_KEY = 'widget_visited';
 
 export default class Item extends LightningElement {
+    @api uid;
     @api text;
     @api type;
     @api url;
+    visited = [];
+
+    // Get visit data from local storage
+    connectedCallback() {
+        var visitedString = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+        if (visitedString !== null) {
+            this.visited = visitedString.split(',');
+        }
+    }
 
     // Title element CSS class depends on item type
     get titleClass() {
@@ -15,5 +27,18 @@ export default class Item extends LightningElement {
         }
 
         return 'title title-' + iconName;
+    }
+
+    // Open link, record action in localstorage
+    handleLinkClick(e) {
+        var itemId = e.target.dataset.id;
+
+        // Update local storage
+        if (this.visited.indexOf(itemId) === -1) {
+            this.visited.push(itemId);
+            localStorage.setItem(LOCAL_STORAGE_KEY, this.visited.join(','));
+        }
+
+        return true;
     }
 }
